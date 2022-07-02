9afc5bf65e2561c1ce8f8d639ea955ada2e24eb6
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-07-03 06:22:05 +0900

    pinataへのファイルアップロードボタンを作りました。

--- client/src/component/pages/Upload.tsx
+++ client/src/component/pages/Upload.tsx
@@ -2,15 +2,22 @@
  * 画像データアップロード画面用コンポーネント
  */
 
+import { Button } from "@mui/material";
+import axios from 'axios';
+import FormData from 'form-data';
 import { ReactElement, useState } from "react";
-import { Button, Input } from "@mui/material";
 import { Link } from 'react-router-dom';
-import axios from 'axios'
-import FormData from 'form-data'
+import '../../App.css';
 import UseStyles from '../common/UseStyles';
-import '../../App.css'
 // WebサーバーのURL
 const baseUrl = "http://localhost:3001";
+const fs = require('fs');
+
+// 設定ファイルから値を読み取る
+const {
+    REACT_APP_API_Key,
+    REACT_APP_API_Secret
+} = process.env;
 
 /**
  * Uploadコンポーネント
@@ -58,6 +65,48 @@ function Upload():ReactElement {
         }
     };
 
+    /**
+     * Pinataへアップロードする際に実行する関数
+     */
+    const pintaUploadFile = async (event:any) => {
+        // リクエストパラメータに詰める名前とデータ本体
+        const { name, files } = event.target;
+        event.target.value = '';
+
+        // APIエンドポイントのベースURL
+        const baseAPIUrl = "https://api.pinata.cloud";
+
+        //送信用のデータを用意する。
+        let postData = new FormData();
+        // 送信データを作成する。
+        /*
+        postData.append('fileName',fileName);
+        postData.append('file',file);
+        console.log("postData:", postData);
+        */
+        postData.append('file', file);
+        postData.append('pinataOptions', '{"cidVersion": 1}');
+        postData.append('pinataMetadata', `{"name": "${fileName}", "keyvalues": {"company": "Pinata"}}`);
+        
+
+        try {
+            // POSTメソッドで送信
+            const res = await axios.post(baseAPIUrl + '/pinning/pinFileToIPFS', postData , {
+                headers: {
+                    'accept': 'application/json',
+                    'pinata_api_key': `${REACT_APP_API_Key}`,
+                    'pinata_secret_api_key': `${REACT_APP_API_Secret}`,
+                    'Content-Type': `multipart/form-data; boundary=${postData}`,
+                },  
+            });
+            console.log(res);
+            alert("ファイルアップロード成功！");
+        } catch (e) {
+            console.error("ファイルアップロード失敗：", e);
+            alert("ファイルアップロード失敗");
+        }
+    };
+
     return (
         <div className="App">
             <p>ファイルアップロード画面</p>
@@ -70,6 +119,10 @@ function Upload():ReactElement {
             <br/><br/>
             <Button variant="contained" onClick={changeUploadFile} color="secondary">
                 アップロード
+            </Button>
+            <br/><br/>
+            <Button variant="outlined" onClick={pintaUploadFile} color="secondary">
+                Pinataへアップロード
             </Button>
             <br/><br/>
             <Button variant="contained" color="success">
